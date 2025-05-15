ed8345f58fac1df152a59487dce35b9d509a3794
Harika <harika.rolla@nationwide.co.uk>
2025-05-15 17:12:26 +0100

    latest changes

--- tests/test_users_repository.py
+++ tests/test_users_repository.py
@@ -1,6 +1,7 @@
 from lib.users_repository import UserRepository
 from lib.user import User
 import pytest
+import bcrypt
 
 def test_create_user(db_connection):
     db_connection.seed("seeds/makersbnb.sql")
@@ -46,7 +47,7 @@ def test_create_user_mismatched_passwords(db_connection):
         assert str(excinfo.value) == "Passwords must match"
 
 
-# retrieving the list of all users 
+#retrieving the list of all users 
 def test_get_all_users(db_connection):
     db_connection.seed("seeds/makersbnb.sql")
     repo = UserRepository(db_connection)
@@ -54,9 +55,17 @@ def test_get_all_users(db_connection):
     users = repo.get_all_users()
 
     assert users == [
-        User(1, "john_doe", "johndoe@gmail.com", "password123"),
-        User(2, "jane_doe", "janedoe@gmail.com", "password456")
+        User(1, "john_doe", "johndoe@gmail.com", "$2b$12$R0zBpJO4u/kNrv7jQFavEesRqXKLFiv1fOhiE2Hb71.9YWZLz3EaG"),
+        User(2, "jane_doe", "janedoe@gmail.com", "$2b$12$aoeO/hoKEgaq2i033emYHOIi.gfKsjUZyDkF25b1PtTeU5.VBaH2.")
     ]
+# def test_get_all_users(db_connection):
+#     db_connection.seed("seeds/makersbnb.sql")
+#     repo = UserRepository(db_connection)
+#     users = repo.get_all_users()
+
+#     # Check password by comparing the hashed value
+#     assert bcrypt.check_password_hash(users[0].password, "password123")
+#     assert bcrypt.check_password_hash(users[1].password, "password456")
 
 #Getting the specific user by their id
 def test_get_user_by_id(db_connection):
@@ -64,7 +73,7 @@ def test_get_user_by_id(db_connection):
     repo = UserRepository(db_connection)
 
     user = repo.get_user(1)
-    assert user == User(1, "john_doe", "johndoe@gmail.com", "password123")
+    assert user == User(1, "john_doe", "johndoe@gmail.com", "$2b$12$R0zBpJO4u/kNrv7jQFavEesRqXKLFiv1fOhiE2Hb71.9YWZLz3EaG")
 
 
 # When a user is removed from the system it no longer exists
@@ -75,7 +84,7 @@ def test_remove_user(db_connection):
     repo.remove_user(1)
     users = repo.get_all_users()
 
-    assert users == [User(2, "jane_doe", "janedoe@gmail.com", "password456")]
+    assert users == [User(2, "jane_doe", "janedoe@gmail.com", "$2b$12$aoeO/hoKEgaq2i033emYHOIi.gfKsjUZyDkF25b1PtTeU5.VBaH2.")]
 
 
 #User can log in if the email and password match
@@ -83,9 +92,9 @@ def test_sign_in_success(db_connection):
     db_connection.seed("seeds/makersbnb.sql")
     repo = UserRepository(db_connection)
 
-    user = repo.sign_in("johndoe@gmail.com", "password123")
+    user = repo.sign_in("johndoe@gmail.com", "$2b$12$R0zBpJO4u/kNrv7jQFavEesRqXKLFiv1fOhiE2Hb71.9YWZLz3EaG")
 
-    assert user == User(1, "john_doe", "johndoe@gmail.com", "password123")
+    assert user == User(1, "john_doe", "johndoe@gmail.com", "$2b$12$R0zBpJO4u/kNrv7jQFavEesRqXKLFiv1fOhiE2Hb71.9YWZLz3EaG")
 
 #User unable to login as the password doesn't match
 def test_sign_in_failure(db_connection):
